17dafbe391212993b02f2f55871a29698f93ac5d
farmertim <52063135+farmertim@users.noreply.github.com>
2021-05-13 10:12:50 +0800

    Update RSA_server.py

--- RSA_server.py
+++ RSA_server.py
@@ -5,7 +5,7 @@ HOST, PORT = '127.0.0.1', 8876
 HOST_2, PORT_2 ='127.0.0.1', 8886
 # get my own private key
 myPrivateKey = get_private_key("C:\\Users\\h702_1\.ssh\\id_rsa")
-caller_id = "107316127" # suppose we do not know who will connect to us
+#caller_id = "107316127" # suppose we do not know who will connect to us
 # set up server
 ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ssocket.bind((HOST, PORT))
@@ -14,6 +14,10 @@ while True:
     conn, addr = ssocket.accept()
     print('connected by ' + str(addr)) # insert capture dialer's id at line below
     # get caller's public key
+    caller=conn.recv(1024).decode()
+    json_translate = json.loads(caller)
+    caller_id=decrypt(json_translate, myPrivateKey)
+
     db_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     db_socket.connect((HOST_2, PORT_2))
     db_socket.send(caller_id.encode())
@@ -23,7 +27,7 @@ while True:
     dialerPublicKey = response[1]
     while response[0] == "200":
         try:
-            indata = conn.recv(1024)
+            indata = conn.recv(1024).decode()
             json_translate = json.loads(indata)
             print("已收到:" + decrypt(json_translate, myPrivateKey))
             if len(indata) == 0:
